342d8e1883cf51a30fbf17de6c43d47d659d7dc8
변찬우 <chan00106@naver.com>
2022-04-18 12:13:01 +0900

    Update directory 구조

---
+++ svelte-study/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  let name = "world";
+  let value = "";
+
+  let count = 0;
+
+  function hello(e) {
+    value = e.target.value;
+  }
+</script>
+
+<main>
+  <h1>hello world</h1>
+  <h1>Hello {name.toUpperCase()}!</h1>
+  <input type="text" {value} on:keypress={hello} />
+  <div>{value}</div>
+  <hr />
+  <h2>{count}</h2>
+  <div>
+    <button on:click={() => count++}>+</button>
+    <button on:click={() => count--}>-</button>
+  </div>
+</main>
+
+<style>
+</style>
