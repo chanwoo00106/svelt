ed13c16af296c32b1aa0fe8153073e2114b1d0b5
변찬우 <chan00106@naver.com>
2022-04-18 12:18:54 +0900

    Add todo

--- todo/src/App.svelte
+++ todo/src/App.svelte
@@ -1,30 +1,75 @@
 <script lang="ts">
-	export let name: string;
+  import { Todo } from "./types/Todo";
+
+  class Todos {
+    todos: Todo[] = [
+      {
+        id: 1,
+        todo: "hello",
+        createAt: new Date(),
+        toggle: false,
+      },
+    ];
+    sequence: number = 1;
+
+    create(todo: string): Todo {
+      const newTodo = {
+        todo,
+        createAt: new Date(),
+        id: ++this.sequence,
+        toggle: false,
+      };
+
+      this.todos.push(newTodo);
+
+      return newTodo;
+    }
+
+    delete(id: number): void {
+      this.todos = this.todos.filter((i) => i.id !== id);
+    }
+  }
+
+  const todos = new Todos();
+
+  console.log(todos.todos);
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main id="wrap">
+  <div class="content">
+    <h1 class="title">Todo list</h1>
+    <ul class="todos">
+      {#each todos.todos as todo (todo.id)}
+        <li>{todo.todo}</li>
+      {/each}
+    </ul>
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  #wrap {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #f1f2f6;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  .content {
+    max-width: 30rem;
+    width: 80%;
+    max-height: 40rem;
+    height: 80%;
+    background: #dfe4ea;
+    border-radius: 1rem;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .title {
+    text-align: center;
+  }
+  .todos {
+    list-style: none;
+  }
 </style>
+
---
+++ todo/src/types/Todo.ts
@@ -0,0 +1,6 @@
+export interface Todo {
+  id: number;
+  todo: string;
+  createAt: Date;
+  toggle: boolean;
+}
