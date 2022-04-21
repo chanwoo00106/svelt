2fc603fb6f1a6b12967c89788b1a6db3a5cb5d8d
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-04-21 23:12:51 +0900

    Add input manage

--- todo/src/App.svelte
+++ todo/src/App.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
+  let value: string = "";
+  const onSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+    console.log(value);
+  };
 </script>
 
-<main id="wrap" />
+<main id="wrap">
+  <div class="content">
+    <h1 class="title">Todo list</h1>
+    <form on:submit={onSubmit}>
+      <input type="text" bind:value />
+    </form>
+  </div>
+</main>
 
 <style>
   #wrap {
@@ -11,5 +23,16 @@
     justify-content: center;
     align-items: center;
     background: #f1f2f6;
+  }
+  .content {
+    max-width: 30rem;
+    width: 80%;
+    max-height: 40rem;
+    height: 80%;
+    background: #e2e2e2;
+    border-radius: 1rem;
+  }
+  .title {
+    text-align: center;
   }
 </style>
